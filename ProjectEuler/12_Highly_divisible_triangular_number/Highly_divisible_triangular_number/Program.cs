6ce9dc4a701ac34376bcd83c8efab0e053ff0397
Luna2409 <54844653+Luna2409@users.noreply.github.com>
2020-07-03 14:29:44 +0200

    Update Project Euler 12

--- ProjectEuler/12_Highly_divisible_triangular_number/Highly_divisible_triangular_number/Program.cs
+++ ProjectEuler/12_Highly_divisible_triangular_number/Highly_divisible_triangular_number/Program.cs
@@ -10,17 +10,16 @@ namespace Highly_divisible_triangular_number
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            int number = 0;
 
-            for (int i = 1; count <= 500; i++)
+
+            /*
+            for (int i = 1; count <= 200; i++)
             {
                 count = 0;
-                int j = 1;
 
-                number = number + i;
+                number += i;
 
-                for (j = 1; j <= number ; j++)
+                for (int j = 1; j <= number ; j++)
                 {
                     if (number % j == 0)
                     {
@@ -29,9 +28,45 @@ namespace Highly_divisible_triangular_number
                 }
 
             }
-
+            
             Console.WriteLine(number.ToString());
             Console.ReadLine();
+
+            int numberOfDivisors;
+            int nod;
+            int sqrt = (int) Math.Sqrt(number);
+            */
+
+            int k = 1;
+            int number = 0;
+            
+            
+
+            while (NumberOfDivisors(number) < 500)
+            {
+                number += k;
+                k++;
+            }
+        }
+
+        private static int NumberOfDivisors(int number)
+        {
+            int numberOfDivisors = 0;
+            int sqrt = (int) Math.Sqrt(number);
+
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    numberOfDivisors += 2;
+                }
+                //Correction for perfect square
+                if (sqrt *sqrt == number)
+                {
+                    numberOfDivisors--;
+                }
+                return numberOfDivisors;
+            }
         }
     }
 }
